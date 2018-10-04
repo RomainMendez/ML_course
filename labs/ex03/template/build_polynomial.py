fc908bbbbbc7c2bd2446a90aa6d67bc9e4181296
Romain <Romain.mendez.rm@gmail.com>
2018-10-04 16:29:04 +0100

    first 2 exercises of week 3 done

--- labs/ex03/template/build_polynomial.py
+++ labs/ex03/template/build_polynomial.py
@@ -7,9 +7,9 @@ import numpy as np
 def build_poly(x, degree):
     """polynomial basis functions for input data x, for j=0 up to j=degree."""
     # ***************************************************
-    # INSERT YOUR CODE HERE
     # polynomial basis function: TODO
     # this function should return the matrix formed
     # by applying the polynomial basis to the input data
     # ***************************************************
-    raise NotImplementedError
+    
+    return x.reshape((-1, 1)) ** np.array([range(0,degree+1)])
